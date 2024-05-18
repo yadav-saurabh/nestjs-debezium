d76196041b54253738764da173bf18c0d2d2e05e
yadav-saurabh <yadav-saurabh@outlook.com>
2024-05-18 15:34:40 +0530

    feat: infra for docker compose & nestjs user module with typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true, // !NOTE: synchronize will change the database as per the schema don't use in PRODUCTION
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
